d460b66f0131d01e0249831b81d9939117a6236b
Jade Ramos <7516524-JadeRamos@users.noreply.replit.com>
2024-10-13 22:54:25 +0000

    Started setting up the GUI for the program itself

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from tkinter import *
 from functools import partial
 
+class Converter:
+
+  def __init__(self):
+
+    self.quiz_frame = Frame(padx=10, pady=10)
+    self.quiz_frame.grid()
+
+    self.quiz_heading = Label(self.quiz_frame,
+                             text="Capital City Quiz",
+                             font=("Arial bold 16"))
+
+    self.quiz_heading.grid(row=0)
+
+#main routine
 if __name__ == "__main__":
   root = Tk()
-  root.title("Temperature Converter")
-  converter = Converter()
+  root.title("Capital City Quiz")
   root.mainloop()
