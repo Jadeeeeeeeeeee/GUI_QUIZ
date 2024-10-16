4ee8e7740e4b56d32f7e370d3110096f978821b7
Jadeeeeeeeeeee <105251276+Jadeeeeeeeeeee@users.noreply.github.com>
2024-10-16 01:25:36 +0000

    fixed the width of the buttons and put the instructions to the right place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+
 class Converter:
 
   def __init__(self):
@@ -16,42 +17,47 @@ class Converter:
     
     self.temp_heading.grid(row=0)
 
-    instructions = "Please select the actions you would like to do. Click start \n to start the quiz, instructions for instructions history to \n view previous quiz results, exit to exit the program"
-                            
+    instructions = "Please select the actions you would like to do. Click start " \
+                   "to start the quiz, instructions for instructions history to" \
+                   "view previous quiz results, exit to exit the program"
 
+                           
     self.temp_instructions = Label(self.temp_frame,
-                                  text=instructions)
+                                  text=instructions,
+                                  wraplength=300,
+                                  width=40,
+                                  justify="left")
     self.temp_instructions.grid(row=1)
 
     self.button_frame = Frame(self.temp_frame)
     self.button_frame.grid(row=4)
 
     self.start_button = Button(self.button_frame,
-                              text="START QUIZ",
+                              text="Start Quiz",
                               bg="#DAE8FC",
                               fg=button_fg,
-                              font=button_font, width=12)
+                              font=button_font, width=10)
     self.start_button.grid(row=0, column=0, padx=5, pady=5)
 
     self.instruct_button = Button(self.button_frame,
-                              text="INSTRUCTIONS",
+                              text="Instructions",
                               bg="#C3ABD0",
                               fg=button_fg,
-                              font=button_font, width=12)
+                              font=button_font, width=10)
     self.instruct_button.grid(row=0, column=1, padx=5, pady=5)
 
     self.history_button = Button(self.button_frame,
-                              text="HISTORY",
+                              text="History",
                               bg="#FFB366",
                               fg=button_fg,
-                              font=button_font, width=12)
+                              font=button_font, width=10)
     self.history_button.grid(row=1, column=0, padx=5, pady=5)
 
     self.quit_button = Button(self.button_frame,
-                              text="QUIT/EXIT",
+                              text="Quit/Exit",
                               bg="#F8CECC",
                               fg=button_fg,
-                              font=button_font, width=12)
+                              font=button_font, width=10)
     self.quit_button.grid(row=1, column=1, padx=5, pady=5)
 
     
