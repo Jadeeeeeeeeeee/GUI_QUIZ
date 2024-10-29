c18014c1748d4936d2135d20c08a32e40474c4fc
Jade Ramos <20233@fraser.school.nz>
2024-10-29 22:00:27 +1300

    Fixed the problem where the start button doesn't create a new tab to run the quiz, it now opens a new for the quiz

--- quiz_GUI_V4.py
+++ quiz_GUI_V4.py
@@ -1,85 +1,10 @@
 from tkinter import *
-from functools import partial
-import random 
-
-class main_GUI:
-
-  def __init__(self):
-
-    button_font = ("Arial 16 bold")
-    button_fg = "#000000"
-
-    self.GUI_frame = Frame(padx=10, pady=10)
-    self.GUI_frame.grid()
-
-    self.GUI_heading = Label(self.GUI_frame,
-                             text="Capital City Quiz",
-                             font=("Arial 16 bold"), )
-
-    self.GUI_heading.grid(row=0)
-
-    instructions = "Please select the actions you would like to do. " 
-
-
-    self.GUI_instructions = Label(self.GUI_frame,
-                                  text=instructions,
-                                  wraplength=350,
-                                  width=50,
-                                  justify="left")
-    self.GUI_instructions.grid(row=1)
-
-
-    self.button_frame = Frame(self.GUI_frame)
-    self.button_frame.grid(row=4)
-
-    self.start_button = Button(self.button_frame,
-                              text="Start",
-                              bg="#DAE8FC",
-                              fg=button_fg,
-                              font=button_font, width=10, command=self.start_quiz)
-    self.start_button.grid(row=0, column=0, padx=5, pady=5)
-
-    self.history_button = Button(self.button_frame,
-                              text="History",
-                              bg="#FFB366",
-                              fg=button_fg,
-                              font=button_font, width=10)
-    self.history_button.grid(row=1, column=0, padx=5, pady=5)
-
-    self.quit_button = Button(self.button_frame,
-                              text="Quit",
-                              bg="#F8CECC",
-                              fg=button_fg,
-                              font=button_font, width=10)
-    self.quit_button.grid(row=1, column=1, padx=5, pady=5)
-
-    self.to_instruct_button = Button(self.button_frame,
-                           text="Instructions",
-                           bg="#C3ABD0",
-                           fg=button_fg,
-                           font=button_font, width=10, command=self.to_instruct)
-
-    self.to_instruct_button.grid(row=0, column=1, padx=5, pady=5)
-
-  def to_instruct(self):
-   DisplayInstructions(self)
-
-  def start_quiz(self):
-    quiz_Gui(self)
- 
+import random
 
 
 class quiz_Gui:
 
-  def __init__(self, partner):
-
-    self.quiz_box = Toplevel()
-
-    partner.start_button.config(state=DISABLED)
-
-    self.quiz_box.protocol('WM_DELETE_WINDOW',
-                           partial(self.close_quiz, partner))
-
+  def __init__(self, root):
 
     self.score = 0
     self.question_number = 0                        
@@ -102,7 +27,8 @@ class quiz_Gui:
     button_font = ("Arial 16 bold")
     button_fg = "#000000"
 
-    self.quiz_frame = Frame(self.quiz_box, padx=10, pady=10)
+
+    self.quiz_frame = Frame(padx=10, pady=10)
     self.quiz_frame.grid()
 
     self.GUI_heading = Label(self.quiz_frame,
@@ -194,63 +120,11 @@ self.option_2[self.question_number],self.option_3[self.question_number]]
        self.choice_b_button.config(state=DISABLED)
        self.choice_c_button.config(state=DISABLED)
        self.choice_d_button.config(state=DISABLED)
-  
-  def close_quiz(self, partner):
-    partner.start_button.config(state=NORMAL)
-    self.quiz_box.destroy()
-
-class DisplayInstructions:
 
 
-  def __init__(self, partner):
-    #setup dialogue box and backround colour
-    backround = "#FFCFAB"
 
-    self.instructions_box = Toplevel()
-
-    #disable help button
-    partner.to_instruct_button.config(state=DISABLED)
-
-    #If users press cross at top, closses help and
-    #'releases' help button
-    self.instructions_box.protocol('WM_DELETE_WINDOW',
-                           partial(self.close_instructions, partner))
-
-    self.instructions_frame = Frame(self.instructions_box, width=300, height=200,
-                           bg=backround)
-    self.instructions_frame.grid()
-
-    self.instructions_heading_label = Label(self.instructions_frame, bg=backround,
-                                   text="Intructions",font="Arial 14 bold")
-
-    self.instructions_heading_label.grid(row=0)
-
-    instruction_text ='''     Functions of the four buttons in the main GUI 
-     the start quiz button: Starts the quiz 
-     the instructions button: Displays the instructions
-     the history button: Displays the history of the quiz
-     the quit button: Exits the program
-     Please enjoy using the program'''
-
-
-    self.instructions_text_label = Label(self.instructions_frame, bg=backround,
-                                text=instruction_text, wraplength=350, width=50, justify="left")
-
-    self.instructions_text_label.grid(row=1, padx=10)
-
-    self.dismiss_button = Button(self.instructions_frame, font=("Arial 12 bold"), 
-                                text="Dismiss", bg="#CC6000", fg="#FFFFFF",
-                                command=partial(self.close_instructions, partner)) 
-    self.dismiss_button.grid(row=2, padx=10, pady=10)
-
-  def close_instructions(self, partner):
-    #Put help button back to normal...
-    partner.to_instruct_button.config(state=NORMAL)
-    self.instructions_box.destroy()
-
-#main routine
 if __name__ == "__main__":
   root = Tk()
   root.title("Capital City Quiz")
-  main_GUI()
+  quiz = quiz_Gui(root)
   root.mainloop()
